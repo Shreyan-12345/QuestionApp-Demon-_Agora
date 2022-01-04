c631c78c3a6d55f1df0e57d96c6873b10f49c3bd
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2022-01-04 18:53:02 +0530

    Initial Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,14 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button nextbutton;
+    private Button A,B,C,D,Next;
+    private TextView QuestionTV, QuestionNumberTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nextbutton=(Button) findViewById(R.id.submit);
+        nextbutton=(Button) findViewById(R.id.idBtnSubmit);
     }
     public void openActivity2(View view)
     {
